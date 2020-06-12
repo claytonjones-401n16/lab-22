8a01135394bbc8373f11bb21579b33c9e9ece52b
Clayton Jones <claytonjonesLMP@gmail.com>
2020-06-11 21:44:54 -0700

    updated readme and jsdocs

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# lab-22
-Repo for seattle-js-401n16
+# Lab 22
+Repo for seattle-js-401n16 lab 22  
+### Author: Clayton Jones
+
+## Deployed Sites
+[Netlify](https://flamboyant-jones-785717.netlify.app/) - 
+[AWS S3](http://401n16-lab-22.s3-website-us-west-1.amazonaws.com/) - 
+[AWS Amplify](https://master.difc8z08gc84d.amplifyapp.com/)  
+  
+## How to use
+Follow any of the links above, and click the `+` and `-` on the page to use the counter.
+
+## UML
+
+![UML](./src/assets/UML.png)
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,16 +6,38 @@ class Counter extends React.Component {
         this.state = { count: 0 };
     }
 
+
+    /**
+     * This function is an event handler than increments the state variable 'count' when called
+     * @function - handleUp
+     * @param {object} e - The event object
+     *
+     */
+
     handleUp = (e) => {
         let count = this.state.count + 1;
         this.updateCounter(count);
     };
+
+    /**
+     * This function is an event handler than decrements the state variable 'count' when called
+     * @function - handleDown
+     * @param {object} e - The event object
+     *
+     */
 
     handleDown = (e) => {
         let count = this.state.count - 1;
         this.updateCounter(count);
     };
 
+    /**
+     * This function updates the state variable. Used inside event handlers handleUp and handleDown
+     * @function - updateCounter
+     * @param {object} e - The event object
+     *
+     */
+    
     updateCounter(count) {
         let polarity = '';
         if (count > 0) {
