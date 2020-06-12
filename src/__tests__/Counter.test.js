1d1c81ed362bad4a5e68e901a3f4713f906c561d
Clayton Jones <claytonjonesLMP@gmail.com>
2020-06-11 20:49:52 -0700

    updated tests on this branch, woops

--- src/__tests__/Counter.test.js
+++ src/__tests__/Counter.test.js
@@ -5,19 +5,27 @@ import { shallow, mount, render } from 'enzyme';
 import Counter from '../components/Counter';
 
 describe('component plus/minus functions correctly', () => {
-  it('increments when the + button is clicked', () => {
+  it('increments state and updates html when the + button is clicked', () => {
     let component = shallow(<Counter />);
     let plus = component.find('.up');
     expect(component.state('count')).toEqual(0);
+    expect(component.find('.count').text()).toBe('0');
     plus.simulate('click');
     expect(component.state('count')).toEqual(1);
+    expect(component.find('.count').text()).toBe('1');
+
   });
 
-  it('decrements when the - button is clicked', () => {
+  it('decrements state and updates html when the - button is clicked', () => {
     let component = shallow(<Counter />);
     let minus = component.find('.down');
     expect(component.state('count')).toEqual(0);
+    expect(component.find('.count').text()).toBe('0');
     minus.simulate('click');
     expect(component.state('count')).toEqual(-1);
+    expect(component.find('.count').text()).toBe('-1');
+
+
   });
 });
+
